c5f81029400dc08fc117eae0fe1130e647134bde
Mikael Fridh <frimik@gmail.com>
2013-05-30 11:13:07 +0200

    site.pp gets osfamily monkeypatch

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,24 @@
-# empty site.pp required by some Puppet versions
+# site.pp is required by some Puppet versions
+
+# Work around missing osfamily fact in Facter < 1.6.1
+if ! $::osfamily {
+  case $::operatingsystem {
+    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'CloudLinux', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
+      $osfamily = 'RedHat'
+    }
+    'ubuntu', 'debian': {
+      $osfamily = 'Debian'
+    }
+    'SLES', 'SLED', 'OpenSuSE', 'SuSE': {
+      $osfamily = 'Suse'
+    }
+    'Solaris', 'Nexenta': {
+      $osfamily = 'Solaris'
+    }
+    default: {
+      $osfamily = $::operatingsystem
+    }
+  }
+}
+
+
